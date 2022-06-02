9e986e835a708bb00ed69a5705bc6e817360a181
Yago Ananias <yago_souza92@yahoo.com.br>
2022-06-01 22:16:43 -0300

    Fixing project starting a new branch

--- src/main/java/com/yagoananias/anime/adapter/dataprovider/service/client/AnimeServiceClient.java
+++
@@ -1,7 +0,0 @@
-package com.yagoananias.anime.adapter.dataprovider.service.client;
-
-import org.springframework.cloud.netflix.feign.FeignClient;
-
-@FeignClient
-public interface AnimeServiceClient {
-}
